06a2346e62a1f4359e60eb1afa854a00a4c10a63
Ali Razmjoo <razmjoo0010@gmail.com>
2026-06-28 19:14:55 +0330

    initial commit: created the repository based on the doc and essentials.

---
+++ backend/src/main/java/com/bazarak/BazarakBackendApplication.java
@@ -0,0 +1,14 @@
+package com.bazarak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarakBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarakBackendApplication.class, args);
+
+	}
+
+}
