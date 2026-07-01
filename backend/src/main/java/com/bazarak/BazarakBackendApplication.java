706b0b7607cb4c4372528378d54af95f4a1648d6
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-01 11:30:30 +0330

    refactor: removed email-verification

--- backend/src/main/java/com/bazarak/BazarakBackendApplication.java
+++ backend/src/main/java/com/bazarak/BazarakBackendApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BazarakBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BazarakBackendApplication.class, args);
-
+		System.out.println("========================================");
+		System.out.println("  🪙  Bazarak Backend is running!  🪙  ");
+		System.out.println("========================================");
 	}
-
 }
