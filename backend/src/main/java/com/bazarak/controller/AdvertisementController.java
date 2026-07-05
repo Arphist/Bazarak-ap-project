57cc8ced0d59e7d2a9913bf33141876d50d9b951
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-05 15:55:39 +0330

    feat: added Advertisement controller -> the endpoint of the ads from backend.

--- backend/src/main/java/com/bazarak/controller/AdvertisementController.java
+++ backend/src/main/java/com/bazarak/controller/AdvertisementController.java
@@ -1,4 +1,22 @@
 package com.bazarak.controller;
 
+import com.bazarak.entity.Advertisement;
+import com.bazarak.entity.User;
+import com.bazarak.service.AdvertisementService;
+import com.bazarak.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/ads")
 public class AdvertisementController {
+
 }
