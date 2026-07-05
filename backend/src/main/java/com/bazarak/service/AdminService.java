31eb616b6254b80e2e76883c74ed6916ee7a55bb
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-05 11:42:17 +0330

    feat: added AdminService to serve the admin.

--- backend/src/main/java/com/bazarak/service/AdminService.java
+++ backend/src/main/java/com/bazarak/service/AdminService.java
@@ -1,4 +1,12 @@
 package com.bazarak.service;
 
+import com.bazarak.entity.User;
+import com.bazarak.exception.user.*;
+import com.bazarak.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AdminService {
+
 }
