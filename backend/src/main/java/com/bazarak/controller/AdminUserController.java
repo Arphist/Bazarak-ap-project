8588bd7ff7600a6c35883b51cf2fc43211c53047
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-05 22:27:47 +0330

    feat: added AdminUserController

---
+++ backend/src/main/java/com/bazarak/controller/AdminUserController.java
@@ -0,0 +1,19 @@
+package com.bazarak.controller;
+
+import com.bazarak.entity.Advertisement;
+import com.bazarak.entity.User;
+import com.bazarak.service.AdvertisementService;
+import com.bazarak.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AdminUserController {
+}
