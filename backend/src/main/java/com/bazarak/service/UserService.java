8e9f0a5ad808a596bb52e20ce70424dec4f80bb6
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-05 16:37:24 +0330

    fix: removed 'isAdmin' method.

--- backend/src/main/java/com/bazarak/service/UserService.java
+++ backend/src/main/java/com/bazarak/service/UserService.java
@@ -210,16 +210,6 @@ public class UserService {
     }
 
     /**
-     * Check if the user is Admin
-     * @param session the http sent as JSON
-     * @return true if the user is Admin, false if not
-     */
-    public boolean isAdmin(HttpSession session) {
-        User user = getCurrentUser(session);
-        return user != null && user.isAdmin();
-    }
-
-    /**
      * Check if the user is owner of the ad
      * @param session the http sent as JSON
      * @return true if the user is owner, false if not
