571b09fcc8969af2c131fbb4f3c08dcc3b4b4617
Mori_Rj <mori.rj.10@gmail.com>
2026-07-06 07:22:26 +0330

    feat: added CityController Class

---
+++ backend/src/main/java/com/bazarak/controller/CityController.java
@@ -0,0 +1,19 @@
+package com.bazarak.controller;
+
+import com.bazarak.entity.City;
+import com.bazarak.service.CityService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/cities")
+public class CityController {
+
+}
