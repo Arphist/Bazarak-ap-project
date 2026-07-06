b404ce8942cd47a0869997bd1088fd8abf054c05
Mori_Rj <mori.rj.10.gmail.com>
2026-07-06 06:44:49 +0330

    feat: added City class

---
+++ backend/src/main/java/com/bazarak/entity/City.java
@@ -0,0 +1,12 @@
+package com.bazarak.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "cities")
+public class City {
+}
