82bd48c41f733d322ad057f983eecb8e91452872
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-08 23:58:23 +0330

    feat: added FavoriteRepository.java to connect to database.

---
+++ backend/src/main/java/com/bazarak/repository/FavoriteRepository.java
@@ -0,0 +1,18 @@
+package com.bazarak.repository;
+
+import com.bazarak.entity.Advertisement;
+import com.bazarak.entity.Favorite;
+import com.bazarak.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+}
