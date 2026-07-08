0ad8e3801a4b623923f0fd60c334cd29281e7999
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-09 00:29:09 +0330

    feat: added favorite exception handler

---
+++ backend/src/main/java/com/bazarak/exception/favorite/InvalidOperationException.java
@@ -0,0 +1,7 @@
+package com.bazarak.exception.favorite;
+
+public class InvalidOperationException extends RuntimeException{
+    public InvalidOperationException(String message){
+        super(message);
+    }
+}
