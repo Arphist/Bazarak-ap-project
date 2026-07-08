bdc8fa99929b44bffc2cf2ddb86117822463bb17
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-09 00:05:18 +0330

    feat: git added FavoriteService

---
+++ backend/src/main/java/com/bazarak/service/FavoriteService.java
@@ -0,0 +1,16 @@
+package com.bazarak.service;
+
+import com.bazarak.entity.Advertisement;
+import com.bazarak.entity.Favorite;
+import com.bazarak.entity.User;
+import com.bazarak.exception.favorite.*;
+import com.bazarak.repository.FavoriteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class FavoriteService {
+}
