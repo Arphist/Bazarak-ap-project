fce4b06dd29059e5ce233b88b24a076a07b4f11c
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-08 23:54:38 +0330

    feat: implemented Favorite class

--- backend/src/main/java/com/bazarak/entity/Favorite.java
+++ backend/src/main/java/com/bazarak/entity/Favorite.java
@@ -5,7 +5,78 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
-
+@Entity
+@Table(name = "favorites", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "ad_id"})
+})
 public class Favorite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ad_id", nullable = false)
+    private Advertisement advertisement;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    // CONSTRUCTORS
+
+    public Favorite() {
+    }
+
+    public Favorite(User user, Advertisement advertisement) {
+        this.user = user;
+        this.advertisement = advertisement;
+    }
+
+    // GETTERS & SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Advertisement getAdvertisement() {
+        return advertisement;
+    }
+
+    public void setAdvertisement(Advertisement advertisement) {
+        this.advertisement = advertisement;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Favorite{" +
+                "id=" + id +
+                ", user=" + (user != null ? user.getUsername() : null) +
+                ", ad=" + (advertisement != null ? advertisement.getTitle() : null) +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
