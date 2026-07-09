f5862839f0505e4f5a84ec2f835a0d6bd0ac0b4b
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-09 19:20:09 +0330

    feat: add ImageService class

---
+++ backend/src/main/java/com/bazarak/service/ImageService.java
@@ -0,0 +1,23 @@
+package com.bazarak.service;
+
+import com.bazarak.entity.Image;
+import com.bazarak.exception.image.*;
+import com.bazarak.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ImageService {
+}
