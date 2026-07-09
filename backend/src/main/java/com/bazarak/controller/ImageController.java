1829f34d6e2e379828261739d315ded544661ac2
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-09 22:58:48 +0330

    feat: added ImageController class

---
+++ backend/src/main/java/com/bazarak/controller/ImageController.java
@@ -0,0 +1,29 @@
+package com.bazarak.controller;
+
+import com.bazarak.entity.Image;
+import com.bazarak.entity.User;
+import com.bazarak.service.ImageService;
+import com.bazarak.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/images")
+public class ImageController {
+}
