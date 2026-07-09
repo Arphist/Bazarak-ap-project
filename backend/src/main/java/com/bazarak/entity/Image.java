5846a0c02e3743c44479febc883439948c3837a7
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-09 18:59:54 +0330

    feat: added Image Class

---
+++ backend/src/main/java/com/bazarak/entity/Image.java
@@ -0,0 +1,12 @@
+package com.bazarak.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "images")
+public class Image {
+}
