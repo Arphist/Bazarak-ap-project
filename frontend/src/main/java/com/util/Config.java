45fb39f2c8ed7217827b1f75870c215dad53fc50
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-10 16:04:56 +0330

    feat: add Config class

---
+++ frontend/src/main/java/com/util/Config.java
@@ -0,0 +1,9 @@
+package com.util;
+
+public class Config {
+    // this must be like my backend URL
+    // For local: "http://localhost:8080/api"
+    // For ngrok: "https://abc123.ngrok.io/api"
+    public static final String BASE_URL = "http://localhost:8080/api";
+    public static final String BASE_IMAGE_URL = "http://localhost:8080";
+}
