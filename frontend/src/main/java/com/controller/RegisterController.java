37fed97e9e48a86c7d4362d3bf3efaf4336232f0
Mori_Rj <mori.rj.10@gmail.com>
2026-07-11 23:26:29 +0330

    feat: implement RegisterController class

--- frontend/src/main/java/com/controller/RegisterController.java
+++ frontend/src/main/java/com/controller/RegisterController.java
@@ -1,4 +1,45 @@
 package com.controller;
 
+import com.model.User;
+import com.service.AuthService;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class RegisterController {
+
+    // ======== FXML FIELDS ========
+
+    @FXML
+    private TextField fullNameField;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private PasswordField confirmPasswordField;
+
+    @FXML
+    private Label errorLabel;
+
+    // ======== STAGE ========
+
+    private Stage primaryStage;
+
+    public void setPrimaryStage(Stage stage) {
+        this.primaryStage = stage;
+    }
 }
