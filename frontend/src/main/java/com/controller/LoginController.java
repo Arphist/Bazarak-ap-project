3e2d381d29ab854d95a3ec78c92f094e6a9a21bb
Mori_Rj <mori.rj.10@gmail.com>
2026-07-11 23:21:19 +0330

    feat: implement LoginController

--- frontend/src/main/java/com/controller/LoginController.java
+++ frontend/src/main/java/com/controller/LoginController.java
@@ -1,4 +1,36 @@
 package com.controller;
 
+import com.model.User;
+import com.service.AuthService;
+import com.util.SessionManager;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class LoginController {
+
+    // ======== FXML FIELDS ========
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Label errorLabel;
+
+    // ======== STAGE ========
+
+    private Stage primaryStage;
+
+    public void setPrimaryStage(Stage stage) {
+        this.primaryStage = stage;
+    }
+
+
 }
