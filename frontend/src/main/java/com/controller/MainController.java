fe191fc92f8fb9771784d105ff5417ac0360ada8
Mori_Rj <mori.rj.10@gmail.com>
2026-07-11 23:53:01 +0330

    feat: implement MainController class

--- frontend/src/main/java/com/controller/MainController.java
+++ frontend/src/main/java/com/controller/MainController.java
@@ -1,4 +1,40 @@
 package com.controller;
 
+import com.model.Advertisement;
+import com.model.User;
+//import com.service.AdService;
+import com.util.SessionManager;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.util.List;
+
 public class MainController {
+
+    // ======== FXML FIELDS ========
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    private ListView<String> adListView;
+
+    @FXML
+    private Label welcomeLabel;
+
+    @FXML
+    private Label errorLabel;
+
+    // ======== STAGE ========
+
+    private Stage primaryStage;
+
+    public void setPrimaryStage(Stage stage) {
+        this.primaryStage = stage;
+    }
 }
