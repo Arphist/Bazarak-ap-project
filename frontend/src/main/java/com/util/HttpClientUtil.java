3a3e32a605a9f3392bc57d31bb93c5b4ee543809
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-14 00:39:46 +0330

    fix: added session cookie handling

--- frontend/src/main/java/com/util/HttpClientUtil.java
+++ frontend/src/main/java/com/util/HttpClientUtil.java
@@ -3,12 +3,14 @@ package com.util;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+import java.net.CookieManager;
 import java.net.http.HttpClient;
 import java.time.Duration;
 
 public class HttpClientUtil {
     private static final HttpClient httpClient = HttpClient.newBuilder()
             .connectTimeout(Duration.ofSeconds(10))
+            .cookieHandler(new CookieManager())
             .build();
 
     private static final ObjectMapper objectMapper = new ObjectMapper()
