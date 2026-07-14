d4982da460be770b38817e3f203c4130570ffd86
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-14 09:47:18 +0330

    feat: implemented AdCell class

--- frontend/src/main/java/com/view/AdCell.java
+++ frontend/src/main/java/com/view/AdCell.java
@@ -1,4 +1,16 @@
 package com.view;
 
-public class AdCell {
+import javafx.scene.control.ListCell;
+import com.model.Advertisement;
+
+public class AdCell extends ListCell<Advertisement> {
+    @Override
+    protected void updateItem(Advertisement ad, boolean empty) {
+        super.updateItem(ad, empty);
+        if (empty || ad == null) {
+            setText(null);
+        } else {
+            setText(ad.getTitle() + "\n💰 " + ad.getPrice() + " T");
+        }
+    }
 }
