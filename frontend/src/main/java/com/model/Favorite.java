1e0f99521dbf3dd48c71949156e1f436d461b1e9
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-15 20:35:00 +0330

    feat: implemented Favorite class

--- frontend/src/main/java/com/model/Favorite.java
+++ frontend/src/main/java/com/model/Favorite.java
@@ -1,4 +1,31 @@
 package com.model;
 
 public class Favorite {
+    private Long id;
+    private User user;
+    private Advertisement advertisement;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Advertisement getAdvertisement() {
+        return advertisement;
+    }
+
+    public void setAdvertisement(Advertisement advertisement) {
+        this.advertisement = advertisement;
+    }
 }
