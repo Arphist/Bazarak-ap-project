32f64ab6a849a549e244f18a6605f2e745a37a0c
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-15 11:35:24 +0330

    feat: implemented DataHolder

--- frontend/src/main/java/com/util/DataHolder.java
+++ frontend/src/main/java/com/util/DataHolder.java
@@ -1,4 +1,61 @@
 package com.util;
 
+/**
+ * Holds temporary data passed between pages
+ * Used for passing data like ad ID to detail page
+ */
 public class DataHolder {
+    private static Long selectedAdId;
+    private static Long selectedConversationId;
+    private static Long selectedUserId;
+
+    // AD ID
+
+    public static void setSelectedAdId(Long id) {
+        selectedAdId = id;
+    }
+
+    public static Long getSelectedAdId() {
+        return selectedAdId;
+    }
+
+    public static void clearSelectedAdId() {
+        selectedAdId = null;
+    }
+
+    // CONVERSATION ID
+
+    public static void setSelectedConversationId(Long id) {
+        selectedConversationId = id;
+    }
+
+    public static Long getSelectedConversationId() {
+        return selectedConversationId;
+    }
+
+    public static void clearSelectedConversationId() {
+        selectedConversationId = null;
+    }
+
+    // USER ID
+
+    public static void setSelectedUserId(Long id) {
+        selectedUserId = id;
+    }
+
+    public static Long getSelectedUserId() {
+        return selectedUserId;
+    }
+
+    public static void clearSelectedUserId() {
+        selectedUserId = null;
+    }
+
+    // CLEAR ALL
+
+    public static void clearAll() {
+        selectedAdId = null;
+        selectedConversationId = null;
+        selectedUserId = null;
+    }
 }
