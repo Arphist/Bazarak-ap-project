27fb7437971d226df9dbe3c9153ebf4c1468d302
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-18 19:28:26 +0330

    feat: added 'getRatingsBySeller' and 'getAverageScore' methods

--- frontend/src/main/java/com/service/RatingService.java
+++ frontend/src/main/java/com/service/RatingService.java
@@ -1,4 +1,60 @@
 package com.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.model.Advertisement;
+import com.model.Rating;
+import com.model.User;
+import com.util.Config;
+import com.util.HttpClientUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.*;
+
 public class RatingService {
+    private static final HttpClient httpClient = HttpClientUtil.getHttpClient();
+    private static final ObjectMapper objectMapper = HttpClientUtil.getObjectMapper();
+    private static final String url = Config.BASE_URL + "/ratings";
+
+    public static List<Rating> getRatingsBySeller(Long id) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url + "/seller/" + id))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            return objectMapper.readValue(response.body(), new TypeReference<List<Rating>>() {});
+        } else {
+            Map<String, String> error = objectMapper.readValue(response.body(), Map.class);
+            throw new Exception(error.getOrDefault("error", "Failed to load the rating"));
+        }
+    }
+
+    public static Map<String, Object> getAverageScore(Long id) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url + "/seller/" + id + "/average"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            Map<String, Object> result = objectMapper.readValue(response.body(), Map.class);
+            Map<String, Object> map = new HashMap<>();
+            map.put("sellerId", result.get("sellerId"));
+            map.put("averageScore", result.get("averageScore"));
+            map.put("totalRatings",result.get("totalRatings"));
+
+            return map;
+        }else{
+            Map<String, String> error = objectMapper.readValue(response.body(), Map.class);
+            throw new Exception(error.getOrDefault("error", "Failed to load average rating"));
+        }
+    }
+
 }
