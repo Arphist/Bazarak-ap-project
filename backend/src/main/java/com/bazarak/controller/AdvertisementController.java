02b5e4ad58b58fe8af3ad200387cdd0ea8065078
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-19 16:50:12 +0330

    fix: removed some TODO phrases

--- backend/src/main/java/com/bazarak/controller/AdvertisementController.java
+++ backend/src/main/java/com/bazarak/controller/AdvertisementController.java
@@ -93,7 +93,6 @@ public class AdvertisementController {
             ad.setDescription(request.getDescription());
             ad.setPrice(request.getPrice());
 
-            //TODO: add addImage method
             Advertisement createdAd = advertisementService.createAd(ad, currentUser.getId(), request.getCityId(),request.getCategoryId());
 
             // 4. Return response
@@ -147,7 +146,6 @@ public class AdvertisementController {
             updatedAd.setCity(city);
             updatedAd.setCategory(category);
 
-            //TODO: add addImage method
             Advertisement savedAd = advertisementService.updateAd(id, updatedAd, currentUser.getId());
 
             Map<String, Object> response = new HashMap<>();
