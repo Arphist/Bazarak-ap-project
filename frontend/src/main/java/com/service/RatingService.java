5412dbfa92b0052cc9fe3a3dded7ed8b29c48d9e
Mori_Rj <mori.rj.10@gmail.com>
2026-07-19 08:03:50 +0330

    feat: added Javadoc to RatingService

--- frontend/src/main/java/com/service/RatingService.java
+++ frontend/src/main/java/com/service/RatingService.java
@@ -19,6 +19,13 @@ public class RatingService {
     private static final ObjectMapper objectMapper = HttpClientUtil.getObjectMapper();
     private static final String url = Config.BASE_URL + "/ratings";
 
+    /**
+     * Retrieves all ratings for a specific seller.
+     *
+     * @param id the seller ID
+     * @return list of ratings given to the seller
+     * @throws Exception if the request fails
+     */
     public static List<Rating> getRatingsBySeller(Long id) throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url + "/seller/" + id))
@@ -36,6 +43,13 @@ public class RatingService {
         }
     }
 
+    /**
+     * Retrieves the average rating information for a seller.
+     *
+     * @param id the seller ID
+     * @return a map containing the seller ID, average score, and total number of ratings
+     * @throws Exception if the request fails
+     */
     public static Map<String, Object> getAverageScore(Long id) throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url + "/seller/" + id + "/average"))
@@ -58,6 +72,12 @@ public class RatingService {
         }
     }
 
+    /**
+     * Retrieves all ratings submitted by the currently logged-in user.
+     *
+     * @return list of the current user's ratings
+     * @throws Exception if the request fails
+     */
     public static List<Rating> getMyRatings() throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url + "/my-ratings"))
@@ -75,6 +95,13 @@ public class RatingService {
         }
     }
 
+    /**
+     * Retrieves all ratings for a specific advertisement.
+     *
+     * @param id the advertisement ID
+     * @return list of ratings for the advertisement
+     * @throws Exception if the request fails
+     */
     public static List<Rating> getRatingsByAdvertisement(Long id) throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url + "/advertisement/" + id))
@@ -92,6 +119,13 @@ public class RatingService {
         }
     }
 
+    /**
+     * Creates a new rating for a seller and advertisement.
+     *
+     * @param rating the rating to create
+     * @return the created rating
+     * @throws Exception if the rating cannot be created
+     */
     public static Rating createRating(Rating rating) throws Exception {
         Map<String, Object> requestBody = new HashMap<>();
         requestBody.put("score", rating.getScore());
@@ -115,6 +149,13 @@ public class RatingService {
         }
     }
 
+    /**
+     * Deletes a rating by its ID.
+     *
+     * @param id the rating ID
+     * @return the success message returned by the server
+     * @throws Exception if the delete operation fails
+     */
     public static String deleteRating(Long id) throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url + "/" + id))
