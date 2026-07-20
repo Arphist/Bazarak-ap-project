d4cbaeeacfd9d741de00b84349edc36de1ac013b
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-20 08:15:03 +0330

    feat: added '@JsonIgnoreProperties(ignoreUnknown = true)' annotation

--- frontend/src/main/java/com/model/User.java
+++ frontend/src/main/java/com/model/User.java
@@ -1,7 +1,10 @@
 package com.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.time.LocalDateTime;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
     private Long id;
     private String username;
