d6629fed85da70553d9985a4f80bb4b2ddc9b010
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-21 16:11:01 +0330

    feat: implemented 'AdvertisementSpecification' model

--- frontend/src/main/java/com/model/AdvertisementSpecification.java
+++ frontend/src/main/java/com/model/AdvertisementSpecification.java
@@ -1,4 +1,43 @@
 package com.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AdvertisementSpecification {
+
+    private Long id;
+    private Long advertisementId;
+    private CategorySpecification specification;
+    private String value;
+
+    // Constructors
+    public AdvertisementSpecification() {}
+
+    public AdvertisementSpecification(Long id, CategorySpecification specification, String value) {
+        this.id = id;
+        this.specification = specification;
+        this.value = value;
+    }
+
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Long getAdvertisementId() { return advertisementId; }
+    public void setAdvertisementId(Long advertisementId) { this.advertisementId = advertisementId; }
+
+    public CategorySpecification getSpecification() { return specification; }
+    public void setSpecification(CategorySpecification specification) { this.specification = specification; }
+
+    public String getValue() { return value; }
+    public void setValue(String value) { this.value = value; }
+
+    @Override
+    public String toString() {
+        return "AdvertisementSpecification{" +
+                "id=" + id +
+                ", specification=" + (specification != null ? specification.getName() : "null") +
+                ", value='" + value + '\'' +
+                '}';
+    }
 }
