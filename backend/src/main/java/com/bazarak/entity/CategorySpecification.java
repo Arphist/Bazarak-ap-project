701cd98b8bf4fd006f4f11f230eab893e6cdcba1
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-23 04:47:34 +0330

    feat: added 'JsonIgnore' annotation to avoid stucking in infinte loop

--- backend/src/main/java/com/bazarak/entity/CategorySpecification.java
+++ backend/src/main/java/com/bazarak/entity/CategorySpecification.java
@@ -1,5 +1,6 @@
 package com.bazarak.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
@@ -26,6 +27,7 @@ public class CategorySpecification {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", nullable = false)
+    @JsonIgnore
     private Category category;
 
     // Constructors
