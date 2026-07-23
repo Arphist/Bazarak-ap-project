23eaace96446a6b46adf0b002324b7de420a17f5
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-23 23:10:37 +0330

    fix: fixed 'WebSocketConfig'

--- backend/src/main/java/com/bazarak/config/WebSocketConfig.java
+++ backend/src/main/java/com/bazarak/config/WebSocketConfig.java
@@ -4,25 +4,42 @@ import com.bazarak.controller.ChatWebSocketHandler;
 import com.bazarak.service.ConversationService;
 import com.bazarak.service.UserService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final ConversationService conversationService;
     private final UserService userService;
 
-    public WebSocketConfig(ConversationService conversationService, UserService userService) {
+    public WebSocketConfig(@Lazy ConversationService conversationService, UserService userService) {
         this.conversationService = conversationService;
         this.userService = userService;
     }
 
     @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*");
+    }
+
+    // Register raw WebSocket handler alongside STOMP
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new ChatWebSocketHandler(conversationService, userService), "/ws")
+        registry.addHandler(new ChatWebSocketHandler(conversationService, userService), "/ws/raw")
                 .setAllowedOrigins("*");
     }
 }
