3e42a31f8adca2cd4be7501fa04f3cc0aec6109d
Ali Razmjoo <razmjoo0010@gmail.com>
2026-07-23 23:01:43 +0330

    fix: updated WebSocketConfig to use the raw handler

--- backend/src/main/java/com/bazarak/config/WebSocketConfig.java
+++ backend/src/main/java/com/bazarak/config/WebSocketConfig.java
@@ -1,25 +1,28 @@
 package com.bazarak.config;
 
+import com.bazarak.controller.ChatWebSocketHandler;
+import com.bazarak.service.ConversationService;
+import com.bazarak.service.UserService;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+    private final ConversationService conversationService;
+    private final UserService userService;
+
+    public WebSocketConfig(ConversationService conversationService, UserService userService) {
+        this.conversationService = conversationService;
+        this.userService = userService;
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new ChatWebSocketHandler(conversationService, userService), "/ws")
                 .setAllowedOrigins("*");
-//                .withSockJS() // ← Enable SockJS
     }
 }
